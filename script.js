8454f8eddc0a6f3cc1073aa44d689b0b4dfe71bf
badronova.rita@mail.ru <badronova.rita@mail.ru>
2024-11-16 20:11:40 +0500

    Добавлены файлы в ветке lesson02

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = 'урок 2';
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 75879.6767587;
+let rollback = (Math.random() * 100) + 1;
+let fullPrice = 500000;
+let adaptive = true;
 
-alert("сообщение с любым текстом");
-console.log("еще одно сообщение с любым текстом")
+
+console.log(title);
+console.log(fullPrice);
+console.log(adaptive);
+
+console.log(screens.length);
+
+console.log(`Стоимость верстки экранов ${screenPrice} рублей`);
+console.log(`Стоимость разработки сайта ${fullPrice} рублей`);
+
+console.log(screens.toLowerCase());
+console.log(screens.split(", "));
+
+console.log(fullPrice * (rollback / 100));
