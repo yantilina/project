91e83b3a0e6873dc9ec35cd5467b18f6a0451905
badronova.rita@mail.ru <badronova.rita@mail.ru>
2024-11-28 22:39:26 +0500

    добавлено выполненное задание урока 7

--- lesson05/script.js
+++ lesson05/script.js
@@ -1,41 +1,89 @@
 'use strict';
 
+const appData = {
+    title: '',
+    screens: '',
+    screenPrice: 0,
+    adaptive: true,
+    rollback: 10,
+    allServicePrices: 0,
+    fullPrice: 0,
+    servicePercentPrice: 0,
+    service1: '',
+    service2: '',
+    asking: function () {
+        this.title = prompt("Как называется ваш проект?", "Калькулятор верстки")
+        this.screens = prompt("Какие типы экранов нужно разработать?", "Простые, сложные")
 
-let allServicePrices
-let service1
-let service2
+        do {
+            this.screenPrice = prompt('Сколько будет стоить данная работа?')
+        } while (!this.isNumber(this.screenPrice))
 
-const isNumber = function (num) {
-    return !isNaN(parseFloat(num)) && isFinite(num)
-}
+        this.adaptive = confirm('Нужен ли адаптив на сайте')
+    },
 
-const asking = function () {
-    screenPrise = prompt('Сколько будет стоить данная работа?')
-    while (!isNumber(screenPrise)) {
-        screenPrise = prompt('Сколько будет стоить данная работа?')
-    }
-}
+    isNumber: function (num) {
+        return !isNaN(parseFloat(num)) && isFinite(num)
+    },
 
-const getAllServicePrices = function () {
-    let sum = 0;
-    for (let i = 0; i < 2; i++) {
-        if (i === 0) {
-            service1 = prompt("Какой дополнительный тип услуги нужен?")
-        } else if (i === 1) {
-            service2 = prompt("Какой дополнительный тип услуги нужен?")
+    getAllServicePrices: function () {
+        let sum = 0;
+
+        for (let i = 0; i < 2; i++) {
+            let price = 0
+            if (i === 0) {
+                this.service1 = prompt("Какой дополнительный тип услуги нужен?")
+            } else if (i === 1) {
+                this.service2 = prompt("Какой дополнительный тип услуги нужен?")
+            }
+
+            do {
+                price = prompt("Сколько это будет стоить?")
+            } while (!this.isNumber(price))
+
+            sum += +price;
         }
-        sum += +prompt("Сколько это будет стоить?")
+        return sum
+    },
+
+    getFullPrice: function () {
+        return +this.screenPrice + this.allServicePrices
+    },
+
+    getServicePercentPrice: function () {
+        return this.fullPrice - (this.fullPrice * (this.rollback / 100))
+    },
+
+    getTitle: function (price) {
+        return this.title.trim()[0].toUpperCase() + this.title.trim().substring(1).toLowerCase()
+    },
+
+    getRollbackMessage: function (price) {
+        if (price >= 30000) {
+            return "Даем скидку в 10%"
+        } else if (price >= 15000 && price < 30000) {
+            return 'Даем скидку в 5%'
+        } else if (price < 15000 && price >= 0) {
+            return 'Скидка не предусмотрена'
+        } else {
+            return 'Что-то пошло не так'
+        }
+    },
+    start: function () {
+        this.asking();
+        this.allServicePrices = this.getAllServicePrices();
+        this.fullPrice = this.getFullPrice();
+        this.servicePercentPrice = this.getServicePercentPrice();
+        this.title = this.getTitle();
+        this.logger();
+    },
+    logger: function () {
+        console.log(this.fullPrice);
+        console.log(this.servicePercentPrice);
+        for (let key in this) {
+            console.log(key);
+        }
     }
-    return sum
 }
 
-allServicePrices = getAllServicePrices()
-
-console.log("allServicePrices", allServicePrices)
-
-
-
-
-
-
-
+appData.start();
